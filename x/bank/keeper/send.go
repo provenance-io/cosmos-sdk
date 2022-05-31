f560a837d83bfe76a6b6d25809015f0b8594a95f
Daniel Wedul <dwedul@figure.com>
2022-05-31 14:56:04 -0600

    Pull in changes from dwedul/11859-send-disabled-change that move the SendEnabled info from Params to state.

--- x/bank/keeper/send.go
+++ x/bank/keeper/send.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -19,6 +20,13 @@ type SendKeeper interface {
 
 	GetParams(ctx sdk.Context) types.Params
 	SetParams(ctx sdk.Context, params types.Params)
+
+	IsSendEnabledDenom(ctx sdk.Context, denom string) bool
+	SetSendEnabled(ctx sdk.Context, denom string, value bool)
+	SetAllSendEnabled(ctx sdk.Context, sendEnableds []*types.SendEnabled)
+	DeleteSendEnabled(ctx sdk.Context, denom string)
+	IterateSendEnabledEntries(ctx sdk.Context, cb func(denom string, sendEnabled bool) (stop bool))
+	GetAllSendEnabledEntries(ctx sdk.Context) []types.SendEnabled
 
 	IsSendEnabledCoin(ctx sdk.Context, coin sdk.Coin) bool
 	IsSendEnabledCoins(ctx sdk.Context, coins ...sdk.Coin) error
@@ -64,12 +72,16 @@ func (k BaseSendKeeper) GetParams(ctx sdk.Context) (params types.Params) {
 
 // SetParams sets the total set of bank parameters.
 func (k BaseSendKeeper) SetParams(ctx sdk.Context, params types.Params) {
-	k.paramSpace.SetParamSet(ctx, &params)
+	if len(params.SendEnabled) > 0 {
+		k.SetAllSendEnabled(ctx, params.SendEnabled)
+	}
+	p := types.NewParams(params.DefaultSendEnabled)
+	k.paramSpace.SetParamSet(ctx, &p)
 }
 
 // InputOutputCoins performs multi-send functionality. It accepts a series of
 // inputs that correspond to a series of outputs. It returns an error if the
-// inputs and outputs don't lineup or if any single transfer of tokens fails.
+// inputs and outputs don't line up or if any single transfer of tokens fails.
 func (k BaseSendKeeper) InputOutputCoins(ctx sdk.Context, inputs []types.Input, outputs []types.Output) error {
 	// Safety check ensuring that when sending coins the keeper must maintain the
 	// Check supply invariant and validity of Coins.
@@ -270,9 +282,22 @@ func (k BaseSendKeeper) setBalance(ctx sdk.Context, addr sdk.AccAddress, balance
 // any of the coins are not configured for sending.  Returns nil if sending is enabled
 // for all provided coin
 func (k BaseSendKeeper) IsSendEnabledCoins(ctx sdk.Context, coins ...sdk.Coin) error {
+	if len(coins) == 0 {
+		return nil
+	}
+	store := ctx.KVStore(k.storeKey)
+	haveDefault := false
+	var defaultVal bool
+	getDefault := func() bool {
+		if !haveDefault {
+			defaultVal = k.GetParams(ctx).DefaultSendEnabled
+			haveDefault = true
+		}
+		return defaultVal
+	}
 	for _, coin := range coins {
-		if !k.IsSendEnabledCoin(ctx, coin) {
-			return sdkerrors.Wrapf(types.ErrSendDisabled, "%s transfers are currently disabled", coin.Denom)
+		if !k.getSendEnabledOrDefault(store, coin.Denom, getDefault) {
+			return types.ErrSendDisabled.Wrapf("%s transfers are currently disabled", coin.Denom)
 		}
 	}
 	return nil
@@ -280,11 +305,111 @@ func (k BaseSendKeeper) IsSendEnabledCoins(ctx sdk.Context, coins ...sdk.Coin) e
 
 // IsSendEnabledCoin returns the current SendEnabled status of the provided coin's denom
 func (k BaseSendKeeper) IsSendEnabledCoin(ctx sdk.Context, coin sdk.Coin) bool {
-	return k.GetParams(ctx).SendEnabledDenom(coin.Denom)
+	return k.IsSendEnabledDenom(ctx, coin.Denom)
 }
 
 // BlockedAddr checks if a given address is restricted from
 // receiving funds.
 func (k BaseSendKeeper) BlockedAddr(addr sdk.AccAddress) bool {
 	return k.blockedAddrs[addr.String()]
+}
+
+// IsSendEnabledDenom returns the current SendEnabled status of the provided denom.
+func (k BaseSendKeeper) IsSendEnabledDenom(ctx sdk.Context, denom string) bool {
+	return k.getSendEnabledOrDefault(ctx.KVStore(k.storeKey), denom, func() bool { return k.GetParams(ctx).DefaultSendEnabled })
+}
+
+// SetSendEnabled sets the SendEnabled flag for a denom to the provided value.
+func (k BaseSendKeeper) SetSendEnabled(ctx sdk.Context, denom string, value bool) {
+	store := ctx.KVStore(k.storeKey)
+	k.setSendEnabledEntry(store, denom, value)
+}
+
+// SetAllSendEnabled sets all the provided SendEnabled entries in the bank store.
+func (k BaseSendKeeper) SetAllSendEnabled(ctx sdk.Context, sendEnableds []*types.SendEnabled) {
+	store := ctx.KVStore(k.storeKey)
+	for _, se := range sendEnableds {
+		k.setSendEnabledEntry(store, se.Denom, se.Enabled)
+	}
+}
+
+// setSendEnabledEntry sets SendEnabled for the given denom to the give value in the provided store.
+func (k BaseSendKeeper) setSendEnabledEntry(store sdk.KVStore, denom string, value bool) {
+	key := types.CreateSendEnabledKey(denom)
+	val := types.ToBoolB(value)
+	store.Set(key, []byte{val})
+}
+
+// DeleteSendEnabled deletes a SendEnabled flag for a denom.
+func (k BaseSendKeeper) DeleteSendEnabled(ctx sdk.Context, denom string) {
+	store := ctx.KVStore(k.storeKey)
+	store.Delete(types.CreateSendEnabledKey(denom))
+}
+
+// getSendEnabledPrefixStore gets a prefix store for the SendEnabled entries.
+func (k BaseSendKeeper) getSendEnabledPrefixStore(ctx sdk.Context) sdk.KVStore {
+	return prefix.NewStore(ctx.KVStore(k.storeKey), types.SendEnabledPrefix)
+}
+
+// IterateSendEnabledEntries iterates over all the SendEnabled entries.
+func (k BaseSendKeeper) IterateSendEnabledEntries(ctx sdk.Context, cb func(denom string, sendEnabled bool) bool) {
+	seStore := k.getSendEnabledPrefixStore(ctx)
+
+	iterator := seStore.Iterator(nil, nil)
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		denom := string(iterator.Key())
+		val := types.IsTrueB(iterator.Value())
+		if cb(denom, val) {
+			break
+		}
+	}
+}
+
+// GetAllSendEnabledEntries gets all the SendEnabled entries that are stored.
+// Any denoms not returned use the default value (set in Params).
+func (k BaseSendKeeper) GetAllSendEnabledEntries(ctx sdk.Context) []types.SendEnabled {
+	var rv []types.SendEnabled
+	k.IterateSendEnabledEntries(ctx, func(denom string, sendEnabled bool) bool {
+		rv = append(rv, types.SendEnabled{Denom: denom, Enabled: sendEnabled})
+		return false
+	})
+	return rv
+}
+
+// getSendEnabled returns whether send is enabled and whether that flag was set for a denom.
+//
+// Example usage:
+//    store := ctx.KVStore(k.storeKey)
+//    sendEnabled, found := getSendEnabled(store, "atom")
+//    if !found {
+//        sendEnabled = DefaultSendEnabled
+//    }
+func (k BaseSendKeeper) getSendEnabled(store sdk.KVStore, denom string) (bool, bool) {
+	key := types.CreateSendEnabledKey(denom)
+	if !store.Has(key) {
+		return false, false
+	}
+	v := store.Get(key)
+	if len(v) != 1 {
+		return false, false
+	}
+	switch v[0] {
+	case types.TrueB:
+		return true, true
+	case types.FalseB:
+		return false, true
+	default:
+		return false, false
+	}
+}
+
+// getSendEnabledOrDefault gets the send_enabled value for a denom. If it's not in the store, this will return the result of the getDefault function.
+func (k BaseSendKeeper) getSendEnabledOrDefault(store sdk.KVStore, denom string, getDefault func() bool) bool {
+	sendEnabled, found := k.getSendEnabled(store, denom)
+	if found {
+		return sendEnabled
+	}
+	return getDefault()
 }
