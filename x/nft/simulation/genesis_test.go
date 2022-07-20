d0c9672843df5eeacad7d9b6ec6728de26b67bb4
Daniel Wedul <github@wedul.com>
2022-07-20 11:10:47 -0600

    Use sdk.NewInt instead of the new cosmossdk.io/math library (so that library doesn't need to be added yet) in x/nft/simulation/genesis_test.go.

--- x/nft/simulation/genesis_test.go
+++ x/nft/simulation/genesis_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/simapp"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
@@ -27,7 +26,7 @@ func TestRandomizedGenState(t *testing.T) {
 		Rand:         r,
 		NumBonded:    3,
 		Accounts:     simtypes.RandomAccounts(r, 3),
-		InitialStake: sdkmath.NewInt(1000),
+		InitialStake: sdk.NewInt(1000),
 		GenState:     make(map[string]json.RawMessage),
 	}
 
