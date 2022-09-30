8c4c9bc95c2155505eeb3665f2131c8a8527c0a0
Ergels Gaxhaj <egaxhaj@provenance.io>
2022-09-30 13:48:05 -0500

    update proto and services

--- simapp/app.go
+++ simapp/app.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"github.com/cosmos/cosmos-sdk/streaming"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -21,6 +20,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"
 	nodeservice "github.com/cosmos/cosmos-sdk/client/grpc/node"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/grpc/tmservice"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
@@ -29,6 +29,7 @@ import (
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/streaming"
 	"github.com/cosmos/cosmos-sdk/testutil/testdata"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -235,7 +236,8 @@ func NewSimApp(
 	if enable := cast.ToBool(appOpts.Get(enableKey)); enable {
 		pluginKey := fmt.Sprintf("%s.%s", baseapp.StreamingTomlKey, baseapp.StreamingPluginTomlKey)
 		pluginName := cast.ToString(appOpts.Get(pluginKey))
-		plugin, err := streaming.NewStreamingPlugin(pluginName)
+		logLevel := cast.ToString(appOpts.Get(flags.FlagLogLevel))
+		plugin, err := streaming.NewStreamingPlugin(pluginName, logLevel)
 		if err != nil {
 			tmos.Exit(err.Error())
 		}
