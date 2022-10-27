c343acca0aeb01e2d00cb4da1438630232d1be12
Daniel Wedul <github@wedul.com>
2022-10-27 15:06:07 -0600

    [1046]: Implement the QuarantinedFunds query.

--- x/bank/keeper/grpc_query.go
+++ x/bank/keeper/grpc_query.go
@@ -2,7 +2,6 @@ package keeper
 
 import (
 	"context"
-
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -274,5 +273,69 @@ func (k BaseKeeper) SendEnabled(goCtx context.Context, req *types.QuerySendEnabl
 			return nil, status.Error(codes.Internal, err.Error())
 		}
 	}
+	return resp, nil
+}
+
+func (k BaseKeeper) QuarantinedFunds(goCtx context.Context, req *types.QueryQuarantinedFundsRequest) (*types.QueryQuarantinedFundsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+	if len(req.FromAddress) > 0 && len(req.ToAddress) == 0 {
+		return nil, status.Error(codes.InvalidArgument, "to_address cannot be empty when from_address is not")
+	}
+
+	var toAddr, fromAddr sdk.AccAddress
+	var err error
+	if len(req.ToAddress) > 0 {
+		toAddr, err = sdk.AccAddressFromBech32(req.ToAddress)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "invalid to address: %s", err.Error())
+		}
+	}
+	if len(req.ToAddress) > 0 {
+		fromAddr, err = sdk.AccAddressFromBech32(req.FromAddress)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "invalid from address: %s", err.Error())
+		}
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	resp := &types.QueryQuarantinedFundsResponse{}
+
+	if len(toAddr) > 0 && len(fromAddr) > 0 {
+		qr := k.GetQuarantineRecord(ctx, toAddr, fromAddr)
+		qf := qr.AsQuarantinedFunds(toAddr, fromAddr)
+		resp.QuarantinedFunds = append(resp.QuarantinedFunds, qf)
+		resp.Pagination = &query.PageResponse{
+			NextKey: nil,
+			Total:   1,
+		}
+	} else {
+		store := k.getQuarantineRecordPrefixStore(ctx, toAddr)
+		resp.Pagination, err = query.FilteredPaginate(
+			store, req.Pagination,
+			func(key, value []byte, accumulate bool) (bool, error) {
+				var qr types.QuarantineRecord
+				qr, err = k.bzToQuarantineRecord(value)
+				if err != nil {
+					return false, err
+				}
+				if qr.Declined {
+					return false, nil
+				}
+				if accumulate {
+					kToAddr, kFromAddr := types.ParseQuarantineRecordKey(key)
+					qf := qr.AsQuarantinedFunds(kToAddr, kFromAddr)
+					resp.QuarantinedFunds = append(resp.QuarantinedFunds, qf)
+				}
+				return true, nil
+			},
+		)
+		if err != nil {
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+	}
+
 	return resp, nil
 }
