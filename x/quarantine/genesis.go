6651b26251c62c78ced33d7ecb11ca4cca47b643
Daniel Wedul <github@wedul.com>
2022-11-08 22:41:28 -0700

    [1046]: Get rid of GetGenesisStateFromAppState (not needed). Add indexes to the GenesisState.Validate errors. Add unit tests on the GenesisState stuff.

--- x/quarantine/genesis.go
+++ x/quarantine/genesis.go
@@ -2,27 +2,25 @@ package quarantine
 
 import (
 	"cosmossdk.io/errors"
-	"encoding/json"
-	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // Validate performs basic validation of genesis data returning an error for any failed validation criteria.
 func (gs GenesisState) Validate() error {
-	for _, addr := range gs.QuarantinedAddresses {
+	for i, addr := range gs.QuarantinedAddresses {
 		if _, err := sdk.AccAddressFromBech32(addr); err != nil {
-			return sdkerrors.ErrInvalidAddress.Wrapf("invalid quarantined address: %v", err)
+			return sdkerrors.ErrInvalidAddress.Wrapf("invalid quarantined address[%d]: %v", i, err)
 		}
 	}
-	for _, resp := range gs.AutoResponses {
+	for i, resp := range gs.AutoResponses {
 		if err := resp.Validate(); err != nil {
-			return errors.Wrap(err, "invalid quarantine auto response entry")
+			return errors.Wrapf(err, "invalid quarantine auto response entry[%d]", i)
 		}
 	}
-	for _, funds := range gs.QuarantinedFunds {
+	for i, funds := range gs.QuarantinedFunds {
 		if err := funds.Validate(); err != nil {
-			return errors.Wrap(err, "invalid quarantined funds")
+			return errors.Wrapf(err, "invalid quarantined funds[%d]", i)
 		}
 	}
 	return nil
@@ -40,15 +38,4 @@ func NewGenesisState(quarantinedAddresses []string, autoResponses []*AutoRespons
 // DefaultGenesisState returns a default quarantine module genesis state.
 func DefaultGenesisState() *GenesisState {
 	return NewGenesisState(nil, nil, nil)
-}
-
-// GetGenesisStateFromAppState returns x/quarantine GenesisState given raw application genesis state.
-func GetGenesisStateFromAppState(cdc codec.JSONCodec, appState map[string]json.RawMessage) *GenesisState {
-	var genesisState GenesisState
-
-	if appState[ModuleName] != nil {
-		cdc.MustUnmarshalJSON(appState[ModuleName], &genesisState)
-	}
-
-	return &genesisState
 }
---
+++ x/quarantine/genesis_test.go
@@ -0,0 +1,297 @@
+package quarantine
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func makeCopyOfGenesisState(orig *GenesisState) *GenesisState {
+	if orig == nil {
+		return nil
+	}
+	return &GenesisState{
+		QuarantinedAddresses: makeCopyOfStringSlice(orig.QuarantinedAddresses),
+		AutoResponses:        makeCopyOfAutoResponseEntries(orig.AutoResponses),
+		QuarantinedFunds:     makeCopyOfQuarantinedFundsSlice(orig.QuarantinedFunds),
+	}
+}
+
+func makeCopyOfAutoResponseEntries(orig []*AutoResponseEntry) []*AutoResponseEntry {
+	if orig == nil {
+		return nil
+	}
+	rv := make([]*AutoResponseEntry, len(orig))
+	for i, entry := range orig {
+		rv[i] = makeCopyOfAutoResponseEntry(entry)
+	}
+	return rv
+}
+
+func makeCopyOfAutoResponseEntry(orig *AutoResponseEntry) *AutoResponseEntry {
+	if orig == nil {
+		return nil
+	}
+	return &AutoResponseEntry{
+		ToAddress:   orig.ToAddress,
+		FromAddress: orig.FromAddress,
+		Response:    orig.Response,
+	}
+}
+
+func makeCopyOfQuarantinedFundsSlice(orig []*QuarantinedFunds) []*QuarantinedFunds {
+	if orig == nil {
+		return orig
+	}
+	rv := make([]*QuarantinedFunds, len(orig))
+	for i, qf := range orig {
+		rv[i] = makeCopyOfQuarantinedFunds(qf)
+	}
+	return rv
+}
+
+func TestGenesisState_Validate(t *testing.T) {
+	testAddr0 := makeTestAddr("gsv", 0).String()
+	testAddr1 := makeTestAddr("gsv", 1).String()
+	badAddr := "this1addressisnaughty"
+
+	goodAutoResponse := &AutoResponseEntry{
+		ToAddress:   testAddr0,
+		FromAddress: testAddr1,
+		Response:    AUTO_RESPONSE_ACCEPT,
+	}
+	badAutoResponse := &AutoResponseEntry{
+		ToAddress:   testAddr0,
+		FromAddress: testAddr1,
+		Response:    -10,
+	}
+
+	goodQuarantinedFunds := &QuarantinedFunds{
+		ToAddress:               testAddr0,
+		UnacceptedFromAddresses: []string{testAddr1},
+		Coins:                   coinMakerOK(),
+		Declined:                false,
+	}
+	badQuarantinedFunds := &QuarantinedFunds{
+		ToAddress:               testAddr0,
+		UnacceptedFromAddresses: []string{testAddr1},
+		Coins:                   coinMakerBad(),
+		Declined:                false,
+	}
+
+	tests := []struct {
+		name    string
+		gs      *GenesisState
+		expErrs []string
+	}{
+		{
+			name: "control",
+			gs: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{goodAutoResponse, goodAutoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{goodQuarantinedFunds, goodQuarantinedFunds},
+			},
+			expErrs: nil,
+		},
+		{
+			name:    "empty",
+			gs:      &GenesisState{},
+			expErrs: nil,
+		},
+		{
+			name: "bad first addr",
+			gs: &GenesisState{
+				QuarantinedAddresses: []string{badAddr, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{goodAutoResponse, goodAutoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{goodQuarantinedFunds, goodQuarantinedFunds},
+			},
+			expErrs: []string{"invalid quarantined address[0]"},
+		},
+		{
+			name: "bad second addr",
+			gs: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, badAddr},
+				AutoResponses:        []*AutoResponseEntry{goodAutoResponse, goodAutoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{goodQuarantinedFunds, goodQuarantinedFunds},
+			},
+			expErrs: []string{"invalid quarantined address[1]"},
+		},
+		{
+			name: "bad first auto response",
+			gs: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{badAutoResponse, goodAutoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{goodQuarantinedFunds, goodQuarantinedFunds},
+			},
+			expErrs: []string{"invalid quarantine auto response entry[0]"},
+		},
+		{
+			name: "bad second auto response",
+			gs: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{goodAutoResponse, badAutoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{goodQuarantinedFunds, goodQuarantinedFunds},
+			},
+			expErrs: []string{"invalid quarantine auto response entry[1]"},
+		},
+		{
+			name: "bad first quarantined funds",
+			gs: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{goodAutoResponse, goodAutoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{badQuarantinedFunds, goodQuarantinedFunds},
+			},
+			expErrs: []string{"invalid quarantined funds[0]"},
+		},
+		{
+			name: "bad second quarantined funds",
+			gs: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{goodAutoResponse, goodAutoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{goodQuarantinedFunds, badQuarantinedFunds},
+			},
+			expErrs: []string{"invalid quarantined funds[1]"},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			orig := makeCopyOfGenesisState(tc.gs)
+			var err error
+			testFunc := func() {
+				err = tc.gs.Validate()
+			}
+			assert.NotPanics(t, testFunc, "GenesisState.Validate()")
+			assertErrorContents(t, err, tc.expErrs, "Validate")
+			assert.Equal(t, orig, tc.gs, "GenesisState before and after Validate")
+		})
+	}
+}
+
+func TestNewGenesisState(t *testing.T) {
+	testAddr0 := makeTestAddr("ngs", 0).String()
+	testAddr1 := makeTestAddr("ngs", 1).String()
+
+	autoResponse := &AutoResponseEntry{
+		ToAddress:   testAddr0,
+		FromAddress: testAddr1,
+		Response:    AUTO_RESPONSE_ACCEPT,
+	}
+
+	quarantinedFunds := &QuarantinedFunds{
+		ToAddress:               testAddr0,
+		UnacceptedFromAddresses: []string{testAddr1},
+		Coins:                   coinMakerOK(),
+		Declined:                false,
+	}
+
+	tests := []struct {
+		name  string
+		addrs []string
+		ars   []*AutoResponseEntry
+		qfs   []*QuarantinedFunds
+		exp   *GenesisState
+	}{
+		{
+			name:  "control",
+			addrs: []string{testAddr0, testAddr1},
+			ars:   []*AutoResponseEntry{autoResponse, autoResponse},
+			qfs:   []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			exp: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{autoResponse, autoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			},
+		},
+		{
+			name:  "nil addrs",
+			addrs: nil,
+			ars:   []*AutoResponseEntry{autoResponse, autoResponse},
+			qfs:   []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			exp: &GenesisState{
+				QuarantinedAddresses: nil,
+				AutoResponses:        []*AutoResponseEntry{autoResponse, autoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			},
+		},
+		{
+			name:  "empty addrs",
+			addrs: []string{},
+			ars:   []*AutoResponseEntry{autoResponse, autoResponse},
+			qfs:   []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			exp: &GenesisState{
+				QuarantinedAddresses: []string{},
+				AutoResponses:        []*AutoResponseEntry{autoResponse, autoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			},
+		},
+		{
+			name:  "nil auto responses",
+			addrs: []string{testAddr0, testAddr1},
+			ars:   nil,
+			qfs:   []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			exp: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        nil,
+				QuarantinedFunds:     []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			},
+		},
+		{
+			name:  "empty auto responses",
+			addrs: []string{testAddr0, testAddr1},
+			ars:   []*AutoResponseEntry{},
+			qfs:   []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			exp: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{},
+				QuarantinedFunds:     []*QuarantinedFunds{quarantinedFunds, quarantinedFunds},
+			},
+		},
+		{
+			name:  "nil quarantined funds",
+			addrs: []string{testAddr0, testAddr1},
+			ars:   []*AutoResponseEntry{autoResponse, autoResponse},
+			qfs:   nil,
+			exp: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{autoResponse, autoResponse},
+				QuarantinedFunds:     nil,
+			},
+		},
+		{
+			name:  "empty quarantined funds",
+			addrs: []string{testAddr0, testAddr1},
+			ars:   []*AutoResponseEntry{autoResponse, autoResponse},
+			qfs:   []*QuarantinedFunds{},
+			exp: &GenesisState{
+				QuarantinedAddresses: []string{testAddr0, testAddr1},
+				AutoResponses:        []*AutoResponseEntry{autoResponse, autoResponse},
+				QuarantinedFunds:     []*QuarantinedFunds{},
+			},
+		},
+		{
+			name:  "all empty",
+			addrs: []string{},
+			ars:   []*AutoResponseEntry{},
+			qfs:   []*QuarantinedFunds{},
+			exp: &GenesisState{
+				QuarantinedAddresses: []string{},
+				AutoResponses:        []*AutoResponseEntry{},
+				QuarantinedFunds:     []*QuarantinedFunds{},
+			},
+		},
+		{
+			name:  "DefaultGenesisState",
+			addrs: nil,
+			ars:   nil,
+			qfs:   nil,
+			exp:   DefaultGenesisState(),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := NewGenesisState(tc.addrs, tc.ars, tc.qfs)
+			assert.Equal(t, tc.exp, actual, "NewGenesisState")
+		})
+	}
+}
