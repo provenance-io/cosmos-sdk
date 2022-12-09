0bffcc4aace8f5c2dbc26e80a1c286bf651e6bba
Daniel Wedul <github@wedul.com>
2022-12-09 13:42:44 -0700

    [1046]: Implement the store decoder (for simulation).

--- x/sanction/simulation/decoder.go
+++ x/sanction/simulation/decoder.go
@@ -1,13 +1,25 @@
 package simulation
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/types/kv"
+	"github.com/cosmos/cosmos-sdk/x/sanction/keeper"
 )
 
 func NewDecodeStore(cdc codec.Codec) func(kvA, kvB kv.Pair) string {
 	return func(kvA, kvB kv.Pair) string {
-		// TODO[1046]: Impelement NewDecodeStore
-		panic("not implemented")
+		switch {
+		case bytes.HasPrefix(kvA.Key, keeper.SanctionedPrefix):
+			return fmt.Sprintf("%v\n%v", kvA.Value, kvB.Value)
+
+		case bytes.HasPrefix(kvA.Key, keeper.TemporaryPrefix):
+			return fmt.Sprintf("%v\n%v", kvA.Value, kvB.Value)
+
+		default:
+			panic(fmt.Sprintf("invalid sanction key %X", kvA.Key))
+		}
 	}
 }
