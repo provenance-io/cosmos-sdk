8e3c68558c937ea9ff0eba2ee3db5b75a63d56e6
Daniel Wedul <github@wedul.com>
2022-12-09 17:34:16 -0700

    [1046]: init and export genesis.

--- x/sanction/keeper/genesis.go
+++ x/sanction/keeper/genesis.go
@@ -6,13 +6,30 @@ import (
 )
 
 // InitGenesis updates this keeper's store using the provided GenesisState.
-func (k Keeper) InitGenesis(ctx sdk.Context, genesisState *sanction.GenesisState) {
-	// TODO[1046]: Implement InitGenesis
-	panic("not implemented")
+func (k Keeper) InitGenesis(ctx sdk.Context, genState *sanction.GenesisState) {
+	k.SetParams(ctx, genState.Params)
+
+	var toSanction []sdk.AccAddress
+	for _, addr := range genState.SanctionedAddresses {
+		toSanction = append(toSanction, sdk.MustAccAddressFromBech32(addr))
+	}
+	k.SanctionAddresses(ctx, toSanction...)
 }
 
 // ExportGenesis reads this keeper's entire state and returns it as a GenesisState.
 func (k Keeper) ExportGenesis(ctx sdk.Context) *sanction.GenesisState {
-	// TODO[1046]: Implement ExportGenesis
-	panic("not implemented")
+	params := k.GetParams(ctx)
+	sanctionedAddrs := k.GetAllSanctionedAddresses(ctx)
+	return sanction.NewGenesisState(params, sanctionedAddrs)
+}
+
+// GetAllSanctionedAddresses gets the bech32 string of every account that is sanctioned.
+// This is designed for use with ExportGenesis. See also IterateSanctionedAddresses.
+func (k Keeper) GetAllSanctionedAddresses(ctx sdk.Context) []string {
+	var rv []string
+	k.IterateSanctionedAddresses(ctx, func(addr sdk.AccAddress) bool {
+		rv = append(rv, addr.String())
+		return false
+	})
+	return rv
 }
