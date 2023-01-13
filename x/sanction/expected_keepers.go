95eb5da03fa2256142221efc84d5e60eefbb5402
Daniel Wedul <github@wedul.com>
2023-01-13 15:10:48 -0700

    [1046]: Fix a couple comments copied from the quarantine stuff.

--- x/sanction/expected_keepers.go
+++ x/sanction/expected_keepers.go
@@ -7,14 +7,14 @@ import (
 	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 )
 
-// AccountKeeper defines the account/auth functionality needed from within the quarantine module.
+// AccountKeeper defines the account/auth functionality needed from within the sanction module.
 type AccountKeeper interface {
 	NewAccount(sdk.Context, authtypes.AccountI) authtypes.AccountI
 	GetAccount(sdk.Context, sdk.AccAddress) authtypes.AccountI
 	SetAccount(sdk.Context, authtypes.AccountI)
 }
 
-// BankKeeper defines the bank functionality needed from within the quarantine module.
+// BankKeeper defines the bank functionality needed from within the sanction module.
 type BankKeeper interface {
 	SetSanctionKeeper(keeper banktypes.SanctionKeeper)
 	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
