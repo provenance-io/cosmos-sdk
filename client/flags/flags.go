fbbd2cb502271fa02a2636cf9ddd3c7e92a00fa6
Daniel Wedul <github@wedul.com>
2025-06-24 17:40:45 -0600

    Make the DefaultGasLimit a constant again (back to how the SDK has it).

--- client/flags/flags.go
+++ client/flags/flags.go
@@ -10,13 +10,12 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 )
 
-var DefaultGasLimit uint64 = 200000
-
 const (
 	// DefaultGasAdjustment is applied to gas estimates to avoid tx execution
 	// failures due to state changes that might occur between the tx simulation
 	// and the actual run.
 	DefaultGasAdjustment = 1.0
+	DefaultGasLimit      = 200000
 	GasFlagAuto          = "auto"
 
 	// DefaultKeyringBackend
